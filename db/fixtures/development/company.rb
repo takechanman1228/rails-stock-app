72bb73599f5c58975c585b21e1e29e8410c4d8ff
takechanman1228 <sigur.ku@gmail.com>
2016-07-13 03:31:29 +0900

    市場の基データがおかしかったから暫定的に主要な市場しかみない対策

--- db/fixtures/development/company.rb
+++ db/fixtures/development/company.rb
@@ -9,7 +9,10 @@
 require "csv"
 
 CSV.foreach('db/company.csv', headers: true) do |row|
-  Company.create(:code => row[0], :name => row[1],
-  :market_id => row[2],:category33_id => row[3],
-  :category17_id => row[4],:scale_id => row[5])
+  # 暫定的に主要なマーケットに絞る
+  if row[2].to_i < 7
+    Company.create(:code => row[0], :name => row[1],
+    :market_id => row[2],:category33_id => row[3],
+    :category17_id => row[4],:scale_id => row[5])
+  end
 end
--- db/fixtures/development/market.rb
+++ db/fixtures/development/market.rb
@@ -9,5 +9,8 @@
 require "csv"
 
 CSV.foreach('db/market.csv', headers: true) do |row|
-  Market.create(:code => row[0], :market => row[1])
+  # 暫定的に主要なマーケットに絞る
+  if row[0].to_i  < 7
+    Market.create(:code => row[0], :market => row[1])
+  end
 end
