9b3c257851bba9755113a27d280ca676667d2920
takechanman1228 <sigur.ku@gmail.com>
2016-07-14 14:47:20 +0900

    fix comment form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,11 +5,22 @@ class CommentsController < ApplicationController
   # GET /comments.json
   def index
     @comments = Comment.all
+
+    @company_hash = {}
+    Comment.all.each{|company|
+      @company_hash[company["code"]] = company["company"]
+    }
   end
 
   # GET /comments/1
   # GET /comments/1.json
   def show
+    @comments = Comment.all
+
+    @company_hash = {}
+    Comment.all.each{|company|
+      @company_hash[company["code"]] = company["company"]
+    }
   end
 
   # GET /comments/new
@@ -20,22 +31,6 @@ class CommentsController < ApplicationController
   # GET /comments/1/edit
   def edit
   end
-
-  # POST /comments
-  # POST /comments.json
-  # def create
-  #   @comment = Comment.new(comment_params)
-  #
-  #   respond_to do |format|
-  #     if @comment.save
-  #       format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-  #       format.json { render :show, status: :created, location: @comment }
-  #     else
-  #       format.html { render :new }
-  #       format.json { render json: @comment.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
 
   def create
     @company = Company.find(params[:company_id]) #routingより
